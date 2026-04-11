388cb978b87bd310ee2a37451ca018a1758ba567
chintana-cpu <chintana@iima.ac.in>
2026-04-12 01:05:20 +0530

    Update drive.js

--- api/drive.js
+++ api/drive.js
@@ -4,6 +4,6 @@ export default async function handler(req, res) {
   const GAS = 'https://script.google.com/macros/s/AKfycbxKslEZWlXa1r7Qhc-Gid5T4un9-5OF2T8T7KORZ41yNtqzBp8o5qRIVfJGiYGOM8qX/exec';
   const r = await fetch(`${GAS}?company=${encodeURIComponent(company)}&query=${encodeURIComponent(query)}&format=text`);
   const text = await r.text();
-  res.setHeader('Content-Type', 'text/plain');
-  res.send(text);
+  res.setHeader('Content-Type', 'text/html');
+  res.send(`<!DOCTYPE html><html><body><pre>${text.replace(/</g,'&lt;').replace(/>/g,'&gt;')}</pre></body></html>`);
 }
